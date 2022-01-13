6bd829925e674bd9f76629267704c8d87bdf6267
DorijanH <dhaspl@gmail.com>
2022-01-13 14:56:25 +0100

    fixed Storybook's emotion conflicting with MUI's

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  "stories": [
+  stories: [
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
     "../views/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/react",
-  "core": {
+  framework: "@storybook/react",
+  core: {
     "builder": "webpack5"
   },
+  features: { emotionAlias: false },
   webpackFinal: async (config) => {
     // Add SCSS support for CSS Modules
     // Source link https://github.com/storybookjs/storybook/issues/12464#issuecomment-824107014
@@ -21,7 +22,7 @@ module.exports = {
       include: path.resolve(__dirname, '../'),
       use: ['style-loader', 'css-loader?modules&importLoaders', 'sass-loader'],
     });
-
+    
     return config;
   }
 }
