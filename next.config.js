4de5880422fed0c19cacf1e264f551d639f955b4
DorijanH <dhaspl@gmail.com>
2022-02-02 14:08:52 +0100

    Cleaning up package.json scripts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const { withPlugins } = require('next-compose-plugins');
 
-const withEnv = require('@moxy/next-env')({
-  removePrefixes: true
-});
 const withReactSvg = require('next-react-svg');
 const withSourceMaps = require('@zeit/next-source-maps');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
@@ -15,15 +12,10 @@ const withProdOnlyPlugin = (plugin) => (process.env.NODE_ENV === 'production'
   : plugin);
 
 module.exports = withPlugins([
-  withEnv,
   withProdOnlyPlugin(withSourceMaps),
   withProdOnlyPlugin(withBundleAnalyzer),
   withReactSvg
 ], {
-  env: {
-    LOCAL_SERVER: false,
-    ENV: process.env.NODE_ENV
-  },
   images: {
     domains: ['assets.pokemon.com'],
     loader: 'imgix',
