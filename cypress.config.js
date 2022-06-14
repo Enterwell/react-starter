3134a0ca366f6ef19594e80ddbb590002dda79f4
yoshitaka taniguchi <truck_mos1500@yahoo.co.jp>
2022-06-14 21:30:11 +0900

    fix lint

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const PluginsFile = require('./cypress/plugins');
 
 module.exports = defineConfig({
   screenshotsFolder: '.cypress-pending',
   trashAssetsBeforeRuns: false,
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      return PluginsFile(on, config);
     },
     baseUrl: 'http://localhost:3000',
     specPattern: 'tests/integration/**/*.cy.{js,jsx,ts,tsx}'
@@ -13,8 +14,8 @@ module.exports = defineConfig({
   component: {
     devServer: {
       framework: 'next',
-      bundler: 'webpack',
+      bundler: 'webpack'
     },
-    specPattern: 'tests/component/*.cy.{js,jsx,ts,tsx}',
-  },
-})
+    specPattern: 'tests/component/*.cy.{js,jsx,ts,tsx}'
+  }
+});
