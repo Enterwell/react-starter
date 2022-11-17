1a5731ae48c9ddfa404fef13f709ca98a458dcff
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2022-11-17 13:02:35 +0100

    chore(DX): NextJS config file cleanup
    Removed old deprecated packages: next-compose-plugins and @zeit/next-source-maps which are no longer needed.
    
    Also closes #245

--- next.config.js
+++ next.config.js
@@ -1,32 +1,16 @@
 const path = require('path');
-const { withPlugins } = require('next-compose-plugins');
 
 const withReactSvg = require('next-react-svg');
-const withSourceMaps = require('@zeit/next-source-maps');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 
-const withProdOnlyPlugin = (plugin) => (process.env.NODE_ENV === 'production'
-  ? (w) => w
-  : plugin);
-
-module.exports = withPlugins([
-  withProdOnlyPlugin(withSourceMaps),
-  withProdOnlyPlugin(withBundleAnalyzer),
-  withReactSvg
-], {
-  swcMinify: true,
+const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['assets.pokemon.com'],
-    loader: 'imgix',
-    path: ''
-  },
   include: path.resolve(__dirname, './public/assets'),
-  assetPrefix: process.env.ASSETS_PREFIX ? `/${process.env.ASSETS_PREFIX}` : '',
-  publicRuntimeConfig: {
-    basePath: process.env.ASSETS_PREFIX ? `/${process.env.ASSETS_PREFIX}` : ''
+  productionBrowserSourceMaps: true,
+  images: {
+    domains: ['assets.pokemon.com']
   },
   eslint: {
     dirs: [
@@ -42,4 +26,6 @@ module.exports = withPlugins([
       'view-models', 'views'
     ]
   }
-});
+};
+
+module.exports = withBundleAnalyzer(withReactSvg(nextConfig));
